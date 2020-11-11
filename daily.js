eb81389d329e05fe6a9431ab127629450344d80f
lea-ben <73586854+lea-ben@users.noreply.github.com>
2020-11-11 09:59:40 +0200

    new repository

---
+++ daily.js
@@ -0,0 +1,22 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+console.log(array) ; 
+
+delete array [0];
+array.sort()
+console.log(array)
+
+array [3]= "kiwi"
+console.log(array)
+
+array.splice (0,1)
+console.log(array);
+
+array.reverse ()
+console.log(array)
+
+// 
+
+var array2= ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log (array2[1][1][0]);
